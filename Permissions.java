000b78396e3654af99ef7e4cd60921f9c265d6a7
montlikadani <montlikada@gmail.com>
2017-08-17 11:17:47 +0200

    Add files via upload

---
+++ Permissions.java
@@ -0,0 +1,14 @@
+package hu.montlikadani.TeleportSigns;
+
+import org.bukkit.permissions.Permission;
+
+public class Permissions {
+	
+	public Permission perm = new Permission("teleportsigns.cmd.nocooldown");
+	public Permission perm1 = new Permission("teleportsigns.plugininfo");
+	public Permission perm2 = new Permission("teleportsigns.help");
+	public Permission perm3 = new Permission("teleportsigns.reload");
+	public Permission perm5 = new Permission("teleportsigns.plugindisable");
+	public Permission perm6 = new Permission("teleportsigns.checkip");
+
+}
