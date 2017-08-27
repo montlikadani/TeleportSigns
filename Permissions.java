b840e9780cbb4a3372de09b8244b2931e269128d
montlikadani <montlikada@gmail.com>
2017-08-27 17:59:25 +0200

    Update Permissions.java

--- Permissions.java
+++ Permissions.java
@@ -8,7 +8,10 @@ public class Permissions {
 	public Permission perm1 = new Permission("teleportsigns.plugininfo");
 	public Permission perm2 = new Permission("teleportsigns.help");
 	public Permission perm3 = new Permission("teleportsigns.reload");
-	public Permission perm5 = new Permission("teleportsigns.plugindisable");
-	public Permission perm6 = new Permission("teleportsigns.checkip");
+	public Permission perm4 = new Permission("teleportsigns.plugindisable");
+	public Permission perm5 = new Permission("teleportsigns.checkip");
+	public Permission perm6 = new Permission("teleportsigns.destroy");
+	public Permission perm7 = new Permission("teleportsigns.create");
+	public Permission perm8 = new Permission("teleportsigns.use.nocooldown");
 
 }
