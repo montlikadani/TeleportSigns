98d4ef91a940e549459ed8f8ff37a32a23bef156
montlikadani <montlikada@gmail.com>
2017-12-26 14:45:44 +0100

    Create Permissions.java

---
+++ src/hu/montlikadani/TeleportSigns/Permissions.java
@@ -0,0 +1,18 @@
+package hu.montlikadani.TeleportSigns;
+
+import org.bukkit.permissions.Permission;
+
+public class Permissions {
+
+	public Permission perm1 = new Permission("teleportsigns.plugininfo");
+	public Permission perm2 = new Permission("teleportsigns.help");
+	public Permission perm3 = new Permission("teleportsigns.reload");
+	public Permission perm4 = new Permission("teleportsigns.plugindisable");
+	public Permission perm5 = new Permission("teleportsigns.checkip");
+	public Permission perm6 = new Permission("teleportsigns.destroy");
+	public Permission perm7 = new Permission("teleportsigns.create");
+	public Permission perm8 = new Permission("teleportsigns.use.nocooldown");
+	public Permission perm9 = new Permission("teleportsigns.cmd.tabcomplete");
+	public Permission perm10 = new Permission("teleportsigns.use");
+
+}
