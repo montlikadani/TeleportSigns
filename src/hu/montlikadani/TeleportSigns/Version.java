5b3ecdc87a29206f9989ac7ef227d8608ef9bac4
montlikadani <montlikada@gmail.com>
2018-03-03 15:42:23 +0100

    Add files via upload

---
+++ src/hu/montlikadani/TeleportSigns/Version.java
@@ -0,0 +1,14 @@
+package hu.montlikadani.TeleportSigns;
+
+public class Version {
+    private String name;
+    private String protocol;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+}
