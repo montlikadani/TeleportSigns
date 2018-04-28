b743ad0a9475285ba87cfb04a890caf0be5c10b6
montlikadani <montlikada@gmail.com>
2018-04-28 11:52:02 +0200

    Update ServerPing.java

--- src/hu/montlikadani/TeleportSigns/ServerPing.java
+++ src/hu/montlikadani/TeleportSigns/ServerPing.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.List;
 
 import com.google.gson.Gson;
@@ -19,142 +20,143 @@ public class ServerPing {
 	private int timeout = 2000;
 	private Gson gson = new Gson();
 
-    public void setAddress(InetSocketAddress host) {
-        this.host = host;
-	this.fetching = false;
-    }
+	public void setAddress(InetSocketAddress host) {
+		this.host = host;
+		this.fetching = false;
+	}
 
-    public InetSocketAddress getAddress() {
-        return this.host;
-    }
+	public InetSocketAddress getAddress() {
+		return this.host;
+	}
 
-    public void setTimeout(int timeout) {
-        this.timeout = timeout;
-    }
+	public void setTimeout(int timeout) {
+		this.timeout = timeout;
+	}
 
-    public int getTimeout() {
-        return this.timeout;
-    }
+	public int getTimeout() {
+		return this.timeout;
+	}
 
-    public boolean isFetching() {
-    	return fetching;
-    }
+	public boolean isFetching() {
+		return fetching;
+	}
 
-    public void setFetching(boolean pinging) {
-    	this.fetching = pinging;
-    }
+	public void setFetching(boolean pinging) {
+		this.fetching = pinging;
+	}
 
-    public int readVarInt(DataInputStream in) throws IOException {
-        int i = 0;
-        int j = 0;
-        while (true) {
-            int k = in.readByte();
-            i |= (k & 0x7F) << (j++ * 7);
-            if (j > 5) {
-            	throw new RuntimeException("VarInt too big");
-            }
-            if ((k & 0x80) != 128) {
-            	break;
-            }
-        }
-        return i;
-    }
+	public int readVarInt(DataInputStream in) throws IOException {
+		int i = 0;
+		int j = 0;
+		while (true) {
+			int k = in.readByte();
+			i |= (k & 0x7F) << j++ * 7;
+			if (j > 5) {
+				throw new RuntimeException("VarInt too big");
+			}
+			if ((k & 0x80) != 128) {
+				break;
+			}
+		}
+		return i;
+	}
 
-    public void writeVarInt(DataOutputStream out, int paramInt) throws IOException {
-        while (true) {
-            if ((paramInt & 0xFFFFFF80) == 0) {
-            	out.writeByte(paramInt);
-            	return;
-            }
+	public void writeVarInt(DataOutputStream out, int paramInt) throws IOException {
+		while (true) {
+			if ((paramInt & 0xFFFFFF80) == 0) {
+				out.writeByte(paramInt);
+				return;
+			}
 
-            out.writeByte(paramInt & 0x7F | 0x80);
-            paramInt >>>= 7;
-        }
-    }
+			out.writeByte(paramInt & 0x7F | 0x80);
+			paramInt >>>= 7;
+		}
+	}
 
-    @SuppressWarnings({ "resource", "unused" })
-    public StatusResponse fetchData() throws IOException {
-    	Socket socket = new Socket();
-        OutputStream outputStream;
-        DataOutputStream dataOutputStream;
-        InputStream inputStream;
-        InputStreamReader inputStreamReader;
+	@SuppressWarnings({ "resource", "unused" })
+	public StatusResponse fetchData() throws IOException {
+		Socket socket = new Socket();
+		OutputStream outputStream;
+		DataOutputStream dataOutputStream;
+		InputStream inputStream;
+		InputStreamReader inputStreamReader;
 
-        socket.setSoTimeout(getTimeout());
-        socket.connect(host, getTimeout());
+		socket.setSoTimeout(this.timeout);
+		socket.connect(host, timeout);
 
-        outputStream = socket.getOutputStream();
-        dataOutputStream = new DataOutputStream(outputStream);
+		outputStream = socket.getOutputStream();
+		dataOutputStream = new DataOutputStream(outputStream);
 
-        inputStream = socket.getInputStream();
-        inputStreamReader = new InputStreamReader(inputStream);
+		inputStream = socket.getInputStream();
+		inputStreamReader = new InputStreamReader(inputStream);
 
-        ByteArrayOutputStream b = new ByteArrayOutputStream();
-        DataOutputStream handshake = new DataOutputStream(b);
-        handshake.writeByte(0x00);
-        writeVarInt(handshake, 4);
-        writeVarInt(handshake, this.host.getHostString().length());
-        handshake.writeBytes(this.host.getHostString());
-        handshake.writeShort(this.host.getPort());
-        writeVarInt(handshake, 1);
+		ByteArrayOutputStream b = new ByteArrayOutputStream();
+		DataOutputStream handshake = new DataOutputStream(b);
+		handshake.writeByte(0x00);
+		writeVarInt(handshake, 4);
+		writeVarInt(handshake, this.host.getHostString().length());
+		handshake.writeBytes(this.host.getHostString());
+		handshake.writeShort(this.host.getPort());
+		writeVarInt(handshake, 1);
 
-        writeVarInt(dataOutputStream, b.size());
-        dataOutputStream.write(b.toByteArray());
+		writeVarInt(dataOutputStream, b.size());
+		dataOutputStream.write(b.toByteArray());
 
-        dataOutputStream.writeByte(0x01);
-        dataOutputStream.writeByte(0x00);
-        DataInputStream dataInputStream = new DataInputStream(inputStream);
+		dataOutputStream.writeByte(0x01);
+		dataOutputStream.writeByte(0x00);
+		DataInputStream dataInputStream = new DataInputStream(inputStream);
 		int size = readVarInt(dataInputStream);
-        int id = readVarInt(dataInputStream);
+		int id = readVarInt(dataInputStream);
 
-        if (id == -1) {
-            throw new IOException("Premature end of stream.");
-        }
+		if (id == -1) {
+			throw new IOException("Premature end of stream.");
+		}
 
-        if (id != 0x00) {
-            throw new IOException("Invalid packetID.");
-        }
-        int length = readVarInt(dataInputStream);
+		if (id != 0x00) {
+			throw new IOException("Invalid packetID.");
+		}
+		int length = readVarInt(dataInputStream);
 
-        if (length == -1) {
-            throw new IOException("Premature end of stream.");
-        }
+		if (length == -1) {
+			throw new IOException("Premature end of stream.");
+		}
 
-        if (length == 0) {
-            throw new IOException("Invalid string length.");
-        }
+		if (length == 0) {
+			throw new IOException("Invalid string length.");
+		}
 
-        byte[] in = new byte[length];
-        dataInputStream.readFully(in);
-        String json = new String(in);
+		byte[] in = new byte[length];
+		dataInputStream.readFully(in);
+		//String json = new String(in);
+		String json = new String(in, Charset.forName("utf-8"));
 
-        long now = System.currentTimeMillis();
-        dataOutputStream.writeByte(0x09);
-        dataOutputStream.writeByte(0x01);
-        dataOutputStream.writeLong(now);
+		long now = System.currentTimeMillis();
+		dataOutputStream.writeByte(0x09);
+		dataOutputStream.writeByte(0x01);
+		dataOutputStream.writeLong(now);
 
-        readVarInt(dataInputStream);
-        id = readVarInt(dataInputStream);
-        if (id == -1) {
-            throw new IOException("Premature end of stream.");
-        }
+		readVarInt(dataInputStream);
+		id = readVarInt(dataInputStream);
+		if (id == -1) {
+			throw new IOException("Premature end of stream.");
+		}
 
-        if (id != 0x01) {
-            throw new IOException("Invalid packetID.");
-        }
-        long pingtime = dataInputStream.readLong();
+		if (id != 0x01) {
+			throw new IOException("Invalid packetID.");
+		}
+		long pingtime = dataInputStream.readLong();
 
-        StatusResponse response = gson.fromJson(json, StatusResponse.class);
-        response.setTime((int) (now - pingtime));
+		StatusResponse response = (StatusResponse)gson.fromJson(json, StatusResponse.class);
+		response.setTime((int) (now - pingtime));
 
-        dataOutputStream.close();
-        outputStream.close();
-        inputStreamReader.close();
-        inputStream.close();
-        socket.close();
+		dataOutputStream.close();
+		outputStream.close();
+		inputStreamReader.close();
+		inputStream.close();
+		socket.close();
 
-        return response;
-    }
+		return response;
+	}
 
 	public class StatusResponse {
 		private String description;
