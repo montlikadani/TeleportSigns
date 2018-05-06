411aff8905fe9ff1f0a99365d781b8c6bbc1a199
montlikadani <montlikada@gmail.com>
2018-05-06 12:35:09 +0200

    Fixed /ts listlayouts command not working

--- src/hu/montlikadani/TeleportSigns/Commands.java
+++ src/hu/montlikadani/TeleportSigns/Commands.java
@@ -2,6 +2,8 @@ package hu.montlikadani.TeleportSigns;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -23,7 +25,7 @@ public class Commands implements CommandExecutor, TabCompleter {
 			if (cmd.getName().equalsIgnoreCase("teleportsigns")) {
 				if (args.length == 0) {
 					if (!sender.hasPermission(Permissions.PINFO) && plugin.getConfig().getBoolean("default-can-see-plugin-information") != true) {
-						sender.sendMessage(plugin.colorMsg(plugin.messages.getString("no-permission").replace("%perm%", "teleportsigns.plugininfo").replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+						sender.sendMessage(plugin.defaults(plugin.messages.getString("no-permission").replace("%perm%", "teleportsigns.plugininfo")));
 						return true;
 					}
 					sender.sendMessage("§6§l[§2§lTeleport§e§lSigns§b§l Info§e§l]");
@@ -34,12 +36,12 @@ public class Commands implements CommandExecutor, TabCompleter {
 					return true;
 				} else if (args[0].equalsIgnoreCase("help")) {
 					if (!sender.hasPermission(Permissions.HELP)) {
-						sender.sendMessage(plugin.colorMsg(plugin.messages.getString("no-permission").replace("%perm%", "teleportsigns.help").replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+						sender.sendMessage(plugin.defaults(plugin.messages.getString("no-permission").replace("%perm%", "teleportsigns.help")));
 						return true;
 					}
 					if (args.length > 1) {
 						if (plugin.getConfig().getBoolean("unknown-command-enable")) {
-							sender.sendMessage(plugin.colorMsg(plugin.getConfig().getString("unknown-command").replace("%command%", commandLabel).replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+							sender.sendMessage(plugin.defaults(plugin.getConfig().getString("unknown-command").replace("%command%", commandLabel)));
 							return true;
 						}
 					}
@@ -49,67 +51,75 @@ public class Commands implements CommandExecutor, TabCompleter {
 					return true;
 				} else if (args[0].equalsIgnoreCase("reload") || args[0].equalsIgnoreCase("rl")) {
 					if (!(sender.hasPermission(Permissions.RELOAD) && sender.isOp())) {
-						sender.sendMessage(plugin.colorMsg(plugin.messages.getString("no-permission").replace("%perm%", "teleportsigns.reload + op").replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+						sender.sendMessage(plugin.defaults(plugin.messages.getString("no-permission").replace("%perm%", "teleportsigns.reload + op")));
 						return true;
 					}
 					if (args.length > 1) {
 						if (plugin.getConfig().getBoolean("unknown-command-enable")) {
-							sender.sendMessage(plugin.colorMsg(plugin.getConfig().getString("unknown-command").replace("%command%", commandLabel).replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+							sender.sendMessage(plugin.defaults(plugin.getConfig().getString("unknown-command").replace("%command%", commandLabel)));
 							return true;
 						}
 					}
 					plugin.reload();
-					sender.sendMessage(plugin.colorMsg(plugin.messages.getString("reload-config").replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+					sender.sendMessage(plugin.defaults(plugin.messages.getString("reload-config")));
 					return true;
 				} else if (args[0].equalsIgnoreCase("disable")) {
 					if (!(sender.hasPermission(Permissions.PDISABLE) && sender.isOp())) {
-						sender.sendMessage(plugin.colorMsg(plugin.messages.getString("no-permission").replace("%perm%", "teleportsigns.plugindisable + op").replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+						sender.sendMessage(plugin.defaults(plugin.messages.getString("no-permission").replace("%perm%", "teleportsigns.plugindisable + op")));
 						return true;
 					}
 					if (args.length > 1) {
 						if (plugin.getConfig().getBoolean("unknown-command-enable")) {
-							sender.sendMessage(plugin.colorMsg(plugin.getConfig().getString("unknown-command").replace("%command%", commandLabel).replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+							sender.sendMessage(plugin.defaults(plugin.getConfig().getString("unknown-command").replace("%command%", commandLabel)));
 							return true;
 						}
 					}
 					plugin.getServer().getPluginManager().disablePlugin(plugin);
-					sender.sendMessage(plugin.colorMsg(plugin.messages.getString("warning-disable-command").replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+					sender.sendMessage(plugin.defaults(plugin.messages.getString("warning-disable-command")));
 					return true;
 				} else if (args[0].equalsIgnoreCase("checkip")) {
 					if (!(sender instanceof Player)) {
-						sender.sendMessage(plugin.colorMsg(plugin.messages.getString("no-console-check-IP").replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+						sender.sendMessage(plugin.defaults(plugin.messages.getString("no-console-check-IP")));
 						return true;
 					}
 		  			if (!(sender.hasPermission(Permissions.CHECKIP) && sender.isOp())) {
-		  				sender.sendMessage(plugin.colorMsg(plugin.messages.getString("no-permission").replace("%perm%", "teleportsigns.checkip + op").replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+		  				sender.sendMessage(plugin.defaults(plugin.messages.getString("no-permission").replace("%perm%", "teleportsigns.checkip + op")));
 		  				return true;
 		  			}
 					if (args.length > 1) {
 						if (plugin.getConfig().getBoolean("unknown-command-enable")) {
-							sender.sendMessage(plugin.colorMsg(plugin.getConfig().getString("unknown-command").replace("%command%", commandLabel).replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+							sender.sendMessage(plugin.defaults(plugin.getConfig().getString("unknown-command").replace("%command%", commandLabel)));
 							return true;
 						}
 					}
 					Player p = (Player)sender;
-					sender.sendMessage(plugin.colorMsg(plugin.messages.getString("check-IP-message").replace("%ipaddress%", TeleportSigns.getIP(p) + ":" + TeleportSigns.getPort(p)).replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+					sender.sendMessage(plugin.defaults(plugin.messages.getString("check-IP-message").replace("%ipaddress%", TeleportSigns.getIP(p) + ":" + TeleportSigns.getPort(p))));
 					return true;
 				} else if (args[0].equalsIgnoreCase("listlayouts")) {
 					if (!sender.hasPermission(Permissions.LISTLAYOUT)) {
-						sender.sendMessage(plugin.colorMsg(plugin.messages.getString("no-permission").replace("%perm%", "teleportsigns.listlayouts").replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+						sender.sendMessage(plugin.defaults(plugin.messages.getString("no-permission").replace("%perm%", "teleportsigns.listlayouts")));
 						return true;
 					}
 					if (args.length > 1) {
 						if (plugin.getConfig().getBoolean("unknown-command-enable")) {
-							sender.sendMessage(plugin.colorMsg(plugin.getConfig().getString("unknown-command").replace("%command%", commandLabel).replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+							sender.sendMessage(plugin.defaults(plugin.getConfig().getString("unknown-command").replace("%command%", commandLabel)));
 							return true;
 						}
 					}
-					for (String layouts : plugin.layout.getConfigurationSection("layouts").getKeys(false)) {
-						sender.sendMessage(plugin.colorMsg(plugin.messages.getString("list-layouts").replace("%layouts%", layouts).replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+					try {
+						Set<String> layouts = plugin.layout.getConfigurationSection("layouts").getKeys(false);
+						StringBuilder sb = new StringBuilder();
+						for (String s : layouts) {
+							sb.append(s);
+							sb.append(", ");
+						}
+						sender.sendMessage(plugin.defaults(plugin.messages.getString("list-layouts").replace("%layouts%", sb.toString())));
+					} catch (NullPointerException e) {
+						plugin.logConsole(Level.WARNING, "There are no layouts in the layouts.yml file!");
 					}
 					return true;
 				} else {
-					sender.sendMessage(plugin.colorMsg(plugin.messages.getString("unknown-sub-command").replace("%subcmd%", args[0]).replace("%newline%", "\n").replace("%prefix%", plugin.messages.getString("prefix"))));
+					sender.sendMessage(plugin.defaults(plugin.messages.getString("unknown-sub-command").replace("%subcmd%", args[0])));
 					return true;
 				}
 			}
