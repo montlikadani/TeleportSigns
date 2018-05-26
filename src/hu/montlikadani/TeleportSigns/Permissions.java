33b0e0952655f1dff04735077ff613a76f907641
montlikadani <montlikada@gmail.com>
2018-05-26 11:30:13 +0200

    Added teleportsigns.connect permission
    Other:
    - Removed teleportsigns.checkip permission.

--- src/hu/montlikadani/TeleportSigns/Permissions.java
+++ src/hu/montlikadani/TeleportSigns/Permissions.java
@@ -8,12 +8,12 @@ public class Permissions {
 	public static final Permission HELP = new Permission("teleportsigns.help");
 	public static final Permission RELOAD = new Permission("teleportsigns.reload");
 	public static final Permission PDISABLE = new Permission("teleportsigns.plugindisable");
-	public static final Permission CHECKIP = new Permission("teleportsigns.checkip");
 	public static final Permission DESTROY = new Permission("teleportsigns.destroy");
 	public static final Permission CREATE = new Permission("teleportsigns.create");
 	public static final Permission NOCOOLDOWN = new Permission("teleportsigns.use.nocooldown");
 	public static final Permission TABCOMP = new Permission("teleportsigns.cmd.tabcomplete");
 	public static final Permission USE = new Permission("teleportsigns.use");
 	public static final Permission LISTLAYOUT = new Permission("teleportsigns.listlayouts");
+	public static final Permission CONNECT = new Permission("teleportsigns.connect");
 
 }
