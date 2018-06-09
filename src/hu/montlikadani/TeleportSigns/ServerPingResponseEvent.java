3c5fed52a055ebc075a01d17fa9faa67a3a9cb8b
montlikadani <montlikada@gmail.com>
2018-06-09 10:06:38 +0200

    Cancellable event

--- src/hu/montlikadani/TeleportSigns/ServerPingResponseEvent.java
+++ src/hu/montlikadani/TeleportSigns/ServerPingResponseEvent.java
@@ -1,15 +1,17 @@
 package hu.montlikadani.TeleportSigns;
 
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 import hu.montlikadani.TeleportSigns.ServerPing.StatusResponse;
 
-public class ServerPingResponseEvent extends Event {
+public class ServerPingResponseEvent extends Event implements Cancellable {
 	private static final HandlerList handlers = new HandlerList();
 	private ServerInfo server;
 	private ServerPing ping;
 	private StatusResponse response;
+	private boolean cancelled = false;
 
 	public ServerPingResponseEvent(ServerInfo server, ServerPing ping, StatusResponse response) {
 		this.server = server;
@@ -36,5 +38,13 @@ public class ServerPingResponseEvent extends Event {
 
 	public StatusResponse getResponse() {
 		return response;
+	}
+
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	public void setCancelled(boolean cancelled) {
+		this.cancelled = cancelled;
 	}
 }
